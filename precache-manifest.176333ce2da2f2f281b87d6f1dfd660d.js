90d42df4ef2dcc1f0bf59fa53eafff090cddc61d
KareemHisham <kareem.hisham.ahmed@gmail.com>
2019-10-04 17:53:41 +0200

    Updates

---
+++ precache-manifest.176333ce2da2f2f281b87d6f1dfd660d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda67deeccc771e834dbcfe78b304648",
+    "url": "/Crud-App/index.html"
+  },
+  {
+    "revision": "776bc2b4eaffe033e02a",
+    "url": "/Crud-App/static/css/main.1a48688b.chunk.css"
+  },
+  {
+    "revision": "5d09ddd64d23e705186f",
+    "url": "/Crud-App/static/js/2.de738935.chunk.js"
+  },
+  {
+    "revision": "776bc2b4eaffe033e02a",
+    "url": "/Crud-App/static/js/main.0a4cf36d.chunk.js"
+  },
+  {
+    "revision": "7b39eac24afd08bae3d5",
+    "url": "/Crud-App/static/js/runtime-main.afe24139.js"
+  }
+]);
